f6745112d13e6e61f2b42a1f6efe9dc2ffe2a4f8
Lars Kappert <lars@webpro.nl>
2014-01-02 09:40:16 +0100

    Install Quick Look plugins

--- install.sh
+++ install.sh
@@ -54,6 +54,9 @@ brew cask install transmit
 brew cask install virtualbox
 brew cask install webstorm
 
+# https://github.com/sindresorhus/quick-look-plugins
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webp-quicklook suspicious-package && qlmanage -r
+
 # Update Homebrew, formulae, and packages
 
 brew update
