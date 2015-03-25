ef5e91f13505e637c8a0724290f9e7506d05c245
Lars Kappert <lars@webpro.nl>
2015-03-25 21:52:56 +0100

    Use array for brew(-cask) packages

--- install/brew-cask.sh
+++ install/brew-cask.sh
@@ -1,30 +1,34 @@
 #!/bin/sh
 
-brew cask install alfred
-brew cask install atom
-brew cask install cornerstone
-brew cask install dash
-brew cask install dropbox
-brew cask install firefox
-brew cask install firefox-nightly
-brew cask install flux
-brew cask install glimmerblocker
-brew cask install google-chrome
-brew cask install google-chrome-canary
-brew cask install google-drive
-brew cask install iterm2
-brew cask install kaleidoscope
-brew cask install mou
-brew cask install opera
-brew cask install screenflow
-brew cask install sonos
-brew cask install sourcetree
-brew cask install spotify
-brew cask install sublime-text3
-brew cask install transmit
-brew cask install virtualbox
-brew cask install vlc
-brew cask install webstorm
+apps=(
+    alfred
+    atom
+    cornerstone
+    dash
+    dropbox
+    firefox
+    firefox-nightly
+    flux
+    glimmerblocker
+    google-chrome
+    google-chrome-canary
+    google-drive
+    iterm2
+    kaleidoscope
+    mou
+    opera
+    screenflow
+    sonos
+    sourcetree
+    spotify
+    sublime-text3
+    transmit
+    virtualbox
+    vlc
+    webstorm
+)
+
+brew cask install ${apps[@]}
 
 # Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webp-quicklook suspicious-package && qlmanage -r
--- install/brew.sh
+++ install/brew.sh
@@ -7,25 +7,29 @@ brew upgrade
 
 # Install GNU packages (and override OSX version)
 
-brew install bash-completion
-brew install coreutils
-brew install dockutil
-brew install ffmpeg
-brew install gifsicle
-brew install git
-brew install gnu-sed --default-names
-brew install grep --default-names
-brew install hub
-brew install imagemagick
-brew install jq
-brew install mackup
-brew install node
-brew install peco
-brew install phantomjs
-brew install psgrep
-brew install python
-brew install ssh-copy-id
-brew install svn
-brew install tree
-brew install vim
-brew install wget
+apps=(
+    bash-completion
+    coreutils
+    dockutil
+    ffmpeg
+    gifsicle
+    git
+    gnu-sed --default-names
+    grep --default-names
+    hub
+    imagemagick
+    jq
+    mackup
+    node
+    peco
+    phantomjs
+    psgrep
+    python
+    ssh-copy-id
+    svn
+    tree
+    vim
+    wget
+)
+
+brew install ${apps[@]}
