0f898171c78c92100a974c088b4e2e8ef4834454
Lars Kappert <lars@webpro.nl>
2015-05-28 13:03:10 +0200

    fasd check is-executable

--- system/.fasd.bash
+++ system/.fasd.bash
@@ -1,8 +1,10 @@
 # https://github.com/clvv/fasd#install
 
-fasd_cache="$HOME/.fasd-init-bash"
-if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
-  fasd --init posix-alias bash-hook bash-ccomp bash-ccomp-install >| "$fasd_cache"
+if [ $(is-executable fasd) ]; then
+    fasd_cache="$HOME/.fasd-init-bash"
+    if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
+      fasd --init posix-alias bash-hook bash-ccomp bash-ccomp-install >| "$fasd_cache"
+    fi
+    . "$fasd_cache"
+    unset fasd_cache
 fi
-. "$fasd_cache"
-unset fasd_cache
