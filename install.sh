ba56e93f4950f6682efb65f47294060c58fb5a77
Lars Kappert <lars@webpro.nl>
2015-11-04 22:39:43 +0100

    Add .gemrc and install gems to /usr/local/bin (because SIP)

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ EXTRA_DIR="$HOME/.extra"
 
 ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
 ln -sfv "$DOTFILES_DIR/runcom/.inputrc" ~
+ln -sfv "$DOTFILES_DIR/runcom/.gemrc" ~
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
 
