abc4fbe7346e610db227e13506ac1915051768f1
Lars Kappert <lars@webpro.nl>
2015-12-11 13:57:32 +0100

    Add pip install script + git-sweep

--- install.sh
+++ install.sh
@@ -23,6 +23,7 @@ ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
 . "$DOTFILES_DIR/install/brew.sh"
 . "$DOTFILES_DIR/install/bash.sh"
 . "$DOTFILES_DIR/install/npm.sh"
+. "$DOTFILES_DIR/install/pip.sh"
 
 if [ "$(uname)" == "Darwin" ]; then
     . "$DOTFILES_DIR/install/brew-cask.sh"
