f26a0c4646aaa2919158fe5ab954497543462de9
Lars Kappert <lars@webpro.nl>
2015-12-11 13:57:55 +0100

    Update QL plugin installer

--- install/brew-cask.sh
+++ install/brew-cask.sh
@@ -33,4 +33,4 @@ apps=(
 brew cask install "${apps[@]}"
 
 # Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
-brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webp-quicklook suspicious-package && qlmanage -r
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
