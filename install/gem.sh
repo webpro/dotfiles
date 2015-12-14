c55045c692088105dce3545637d8ab80f120908f
Lars Kappert <lars@webpro.nl>
2015-12-14 09:31:08 +0100

    Stick to Ruby 2.1.7

--- install/gem.sh
+++ install/gem.sh
@@ -3,8 +3,8 @@ brew install gpg2
 gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
 
-rvm install 2.1
-rvm use 2.1
+rvm install 2.1.7
+rvm use 2.1.7 --default
 
 gem install consular
 gem install consular-osx
