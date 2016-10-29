23048f7503cd71af0978a9c27ca278e7d44d2262
Lars Kappert <lars@webpro.nl>
2016-10-29 10:13:36 +0200

    Fix location for some apps in dock

--- osx/dock.sh
+++ osx/dock.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
-dockutil --no-restart --add "$HOME/Applications/Google Chrome.app"
-dockutil --no-restart --add "$HOME/Applications/Firefox.app"
+dockutil --no-restart --add "/Applications/Google Chrome.app"
+dockutil --no-restart --add "/Applications/Firefox.app"
 dockutil --no-restart --add "/Applications/Safari.app"
 dockutil --no-restart --add "/Applications/Mail.app"
 dockutil --no-restart --add "/Applications/Calendar.app"
 dockutil --no-restart --add "/Applications/Utilities/Terminal.app"
 dockutil --no-restart --add "/Applications/System Preferences.app"
 dockutil --no-restart --add "/Applications/Utilities/Console.app"
-dockutil --no-restart --add "$HOME/Applications/Spotify.app"
+dockutil --no-restart --add "/Applications/Spotify.app"
 
 killall Dock
