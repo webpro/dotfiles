c5870313964c5d07031b301e5f9871f62e59231e
Lars Kappert <lars@webpro.nl>
2016-12-04 18:21:40 +0100

    Update ruby version

--- install/gem.sh
+++ install/gem.sh
@@ -3,8 +3,8 @@ brew install gpg2
 gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
 
-rvm install 2.1.7
-rvm use 2.1.7 --default
+rvm install 2.3
+rvm use 2.3 --default
 
 gem install lunchy
 gem install pygmentize
