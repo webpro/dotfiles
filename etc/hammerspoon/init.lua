8d24696ff74c18a692690f6f18b331589e54e1a5
Lars Kappert <lars@webpro.nl>
2017-05-05 22:45:21 +0200

    Add "lock screen" shortcut

--- etc/hammerspoon/init.lua
+++ etc/hammerspoon/init.lua
@@ -3,4 +3,6 @@ mash = {"⌘", "⌥", "⌃"}
 require "apps"
 require "grid"
 
-hs.hotkey.bind(mash, "r", function() hs.reload(); hs.alert.show('Config reloaded'); end)
+hs.hotkey.bind(mash, "r", function() hs.reload(); end)
+hs.hotkey.bind(mash, "a", function() hs.caffeinate.lockScreen(); end)
+hs.alert("Hammerspoon config loaded")
