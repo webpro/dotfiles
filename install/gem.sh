5153bca13794961dd2877bc3a724025c0d6b7df6
Lars Kappert <lars@webpro.nl>
2018-04-08 21:52:36 +0200

    Update ruby to v2.5

--- install/gem.sh
+++ install/gem.sh
@@ -7,8 +7,10 @@ brew install gpg2
 
 \curl -sSL https://get.rvm.io | bash -s stable
 
-rvm install 2.3
-rvm use 2.3 --default
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+
+rvm install 2.5
+rvm use 2.5 --default
 
 gem install lunchy
 gem install pygmentize
