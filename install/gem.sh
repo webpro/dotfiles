520e0ef411a9e0c63e405a1a86eec56671952a41
Lars Kappert <lars@webpro.nl>
2018-10-02 13:07:56 +0200

    Fix rvm install script

--- install/gem.sh
+++ install/gem.sh
@@ -7,6 +7,8 @@ brew install gpg2
 
 \curl -sSL https://get.rvm.io | bash -s stable
 
+. "$HOME/.rvm/scripts/rvm"
+
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 
 rvm install 2.5
