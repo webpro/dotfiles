e1663b554ff538bb4a95263c6415b1ab2c7c7749
Lars Kappert <lars@webpro.nl>
2018-10-10 17:35:00 +0200

    Add default for trackpad tap behaviour

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -116,6 +116,7 @@ defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
 ###############################################################################
 
 # Trackpad: enable tap to click for this user and for the login screen
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
