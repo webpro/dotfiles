cecee61b5e0ad58956724c3347e3536095521dfc
Lars Kappert <lars@webpro.nl>
2018-10-11 23:58:12 +0200

    Move Terminal settings to Mackup

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -343,12 +343,6 @@ defaults write com.apple.terminal StringEncodings -array 4
 defaults write com.apple.terminal "Default Window Settings" -string "Pro"
 defaults write com.apple.terminal "Startup Window Settings" -string "Pro"
 defaults write com.apple.Terminal ShowLineMarks -int 0
-osascript -e "tell application \"Terminal\" to set the font name of window 1 to \"Hack\""
-osascript -e "tell application \"Terminal\" to set the font size of window 1 to 10"
-
-# Disable audible and visual bells
-defaults write com.apple.terminal "Bell" -bool false
-defaults write com.apple.terminal "VisualBell" -bool false
 
 ###############################################################################
 # Activity Monitor                                                            #
