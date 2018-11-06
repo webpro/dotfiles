7fb71684657a0ec8693b8554390962afbe79b375
Dan Golant <dgolant@gmail.com>
2018-11-05 21:39:58 -0500

    Add miscellaneous config file and Itsycal configs

---
+++ macos/defaults-misc.sh
@@ -0,0 +1,5 @@
+# A folder for configuring miscellaneous apps installed in the Brewfile/Caskfile/Gemfile/npmfile
+
+# Set Itsycal to use the default macOS time format and hide the icon
+defaults write com.mowglii.ItsycalApp HideIcon 1
+defaults write com.mowglii.ItsycalApp ClockFormat "E MMM d h:mm a"
