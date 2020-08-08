7994a1b6394db87ba81bddd262bb4113df383c7a
Lars Kappert <lars@webpro.nl>
2020-08-08 08:34:48 +0200

    Fix paths to system apps

--- macos/dock.sh
+++ macos/dock.sh
@@ -2,10 +2,10 @@
 
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/Google Chrome.app"
-dockutil --no-restart --add "/Applications/Mail.app"
-dockutil --no-restart --add "/Applications/Calendar.app"
-dockutil --no-restart --add "/Applications/Utilities/Terminal.app"
-dockutil --no-restart --add "/Applications/System Preferences.app"
+dockutil --no-restart --add "/System/Applications/Mail.app"
+dockutil --no-restart --add "/System/Applications/Calendar.app"
+dockutil --no-restart --add "/System/Applications/Utilities/Terminal.app"
+dockutil --no-restart --add "/System/Applications/System Preferences.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
 
 killall Dock
