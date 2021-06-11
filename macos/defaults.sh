25ceb9d6a1c01feaeac164858306205c137b346a
Jimmy <jimmy@blockchaintp.com>
2021-06-11 13:16:35 -0700

    personalizes macos defaults

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -236,7 +236,7 @@ defaults write com.apple.dock wvous-bl-corner -int 0
 defaults write com.apple.dock wvous-br-corner -int 0
 
 # Don't show recently used applications in the Dock
-defaults write com.Apple.Dock show-recents -bool false
+# defaults write com.Apple.Dock show-recents -bool false
 
 ###############################################################################
 # Mail                                                                        #
