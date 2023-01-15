f5c344cfedf6a5386d029ef5c3ae25c1e5834c3a
Lars Kappert <lars@webpro.nl>
2023-01-15 16:00:40 +0100

    Fix "system settings" name in dockutil script

--- macos/dock.sh
+++ macos/dock.sh
@@ -5,7 +5,7 @@ dockutil --no-restart --add "/Applications/Google Chrome.app"
 dockutil --no-restart --add "/System/Applications/Mail.app"
 dockutil --no-restart --add "/System/Applications/Calendar.app"
 dockutil --no-restart --add "/System/Applications/Utilities/Terminal.app"
-dockutil --no-restart --add "/System/Applications/System Preferences.app"
+dockutil --no-restart --add "/System/Applications/System Settings.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
 
 killall Dock
